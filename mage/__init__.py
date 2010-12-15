43d738a67c907bf458847887ec8a66b3d9625290
Tim Perevezentsev <riffm2005@gmail.com>
2010-12-15 20:43:37 +0300

    initial platform

---
+++ mage/__init__.py
@@ -0,0 +1,2 @@
+from mage import *
+from devserver import *
---
+++ mage/utils.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+class cached_property(object):
+    '''Turns decorated method into caching property (method is called once on
+    first access to property).'''
+
+    def __init__(self, method, name=None):
+        self.method = method
+        self.name = name or method.__name__
+        self.__doc__ = method.__doc__
+
+    def __get__(self, inst, cls):
+        if inst is None:
+            return self
+        result = self.method(inst)
+        setattr(inst, self.name, result)
+        return result
+
+
